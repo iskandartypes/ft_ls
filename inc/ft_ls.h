821faf2372814c5f53db8185cf9e05ddf358079f
naregjan <iskandar.kourkjian.mowad@gmail.com>
2019-04-26 22:28:02 -0700

    cleaned up header, norm'd

--- inc/ft_ls.h
+++ inc/ft_ls.h
@@ -6,55 +6,82 @@
 /*   By: ikourkji <ikourkji@student.42.us.or>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/26 07:22:45 by ikourkji          #+#    #+#             */
-/*   Updated: 2019/04/26 21:33:10 by ikourkji         ###   ########.fr       */
+/*   Updated: 2019/04/26 22:20:06 by ikourkji         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_LS_H
 # define FT_LS_H
 
+/*
+** yes, in the future, I'll start having the appropriate headers in the files
+** and *not* all here like an absolute madman
+*/
+
 # include "ft_printf.h"
 # include "libft.h"
-# include <dirent.h> //opendir, readdir, closedir
+# include <dirent.h>
 # include <stdlib.h>
 # include <sys/types.h>
 # include <pwd.h>
 # include <grp.h>
-# include <uuid/uuid.h> //getpwuid, getgrgid (also prev 4)
-# include <sys/xattr.h> //listxattr, getxattr
-# include <time.h> //time, ctime
-# include <sys/stat.h> //stat, lstat
-# include <unistd.h> //readlink
-# include <stdio.h> //perror
-# include <string.h> //strerror
+# include <uuid/uuid.h>
+# include <time.h>
+# include <sys/stat.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <string.h>
 # include <errno.h>
 
 /*
 ** flags: L/U = lower/upper, followed by letter
 ** instead of overriding by order given, maybe override by cascade for ease?
+**
+** LS_UA	list all except . & ..
+** LS_UF	dir/ , exec*, socket=, symlink@, fifo|
+** LS_UG	colours!
+** LS_UR	REQ recur on subdirs
+** LS_US	sort by size
+** LS_UT	req -l, display complete time info ie Mar 26 20:01:18 2019
+** LS_LA	REQ list all w/ .
+** LS_LF	output not sorted, and -a
+** LS_LG	no username in -l (turns on -l)
+** LS_LI	print inode # (doesn't need -l!)
+** LS_LL	REQ long format
+** LS_LN	disp uid and gid as #; turn on -l
+** LS_LP	 '/' after directories, similar to F but less intense
+** LS_LR	REQ reverse sort order (rev alpha, oldest first, largest last)
+** LS_LS	output blocksize (lists total like -l, doesn't need -l)
+** LS_LT	REQ sort by mod time (most recent first) and then name
+** LS_LU	time of last access instead of last mod for -t sort and -l
+** LS_MU	multiple args (needs header for each)
 */
-# define LS_UA 0x00001	//list all except . & .. DONE
-# define LS_UF 0x00002	//dir/ , exec*, socket=, whiteout% (ignore?), fifo| DONE
-# define LS_UG 0x00004	//colours! DONE
-# define LS_UR 0x00008	//REQ recur on subdirs
-# define LS_US 0x00010	//sort by size DONE
-# define LS_UT 0x00020	//req -l, display complete time info ie Mar 26 20:01:18 2019 DONE
-# define LS_LA 0x00040	//REQ list all w/ . DONE
-# define LS_LF 0x00080	//output not sorted, and -a DONE
-# define LS_LG 0x00100	//no username in -l (turns on -l) DONE
-# define LS_LI 0x00200	//print inode # (doesn't need -l!) DONE
-# define LS_LL 0x00400	//REQ long format DONE
-# define LS_LN 0x00800	//disp uid and gid as #; turn on -l DONE
-# define LS_LP 0x01000	// '/' after directories, similar to F but less intense DONE
-# define LS_LR 0x02000	//REQ reverse sort order (rev alpha, oldest first, largest last)
-# define LS_LS 0x04000	//output blocksize (lists total like -l, doesn't need -l) DONE
-# define LS_LT 0x08000	//REQ sort by mod time (most recent first) and then name DONE
-# define LS_LU 0x10000	//time of last access instead of last mod for -t sort and -l DONE
-# define LS_MU 0x20000	//multiple args (needs header for each) DONE
+
+# define LS_UA 0x00001
+# define LS_UF 0x00002
+# define LS_UG 0x00004
+# define LS_UR 0x00008
+# define LS_US 0x00010
+# define LS_UT 0x00020
+# define LS_LA 0x00040
+# define LS_LF 0x00080
+# define LS_LG 0x00100
+# define LS_LI 0x00200
+# define LS_LL 0x00400
+# define LS_LN 0x00800
+# define LS_LP 0x01000
+# define LS_LR 0x02000
+# define LS_LS 0x04000
+# define LS_LT 0x08000
+# define LS_LU 0x10000
+# define LS_MU 0x20000
 
 /*
 ** tot: size in b / 512 (rounded up) (given by st_blocks no worries)
 ** there's a queue of dirs for -R
+** and a flag that's like "whoa please title my directory" because
+** -R has weird behaviour around directory titles in that the first
+** doesn't have a title but its children do
 */
 
 typedef struct	s_lsdir
@@ -67,24 +94,17 @@ typedef struct	s_lsdir
 	int			r_notfirst;
 }				t_lsdir;
 
-//there'll be a ll of these suckers right here, sorted according to flags
-//like, after being created obvi (and summing themselves for ^tot)
-//need delete func? like to free all the shit inside
-//saving d_name to avoid having to save dirent struct
-//(other info in dirent duped in stats)
 # define SYMBUF 1024
 
 typedef struct	s_lsent
 {
 	char		*name;
 	char		*full_name;
-	struct stat	*stats; //v. important, basically inherits from
-	char		ftype;	//in handy printable fmt
+	struct stat	*stats;
+	char		ftype;
 	char		*perms;
 	char		symbuf[SYMBUF];
 }				t_lsent;
-
-typedef int		(*ls_comp)(t_list*, t_list*);
 
 /*
 ** utils handy for -l and not
